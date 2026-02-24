d1f5d7678f1f408a6ef26aa7762f40464bc9bf2a
manish9548 <baghelmanish4947@gmail.com>
2026-02-25 00:43:41 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import "./App.css";
+
+
+
 const App=() =>{
 
   return <div>
     <nav>
     <div className="logo">
-    
+      <img src="/image/brand_logo.png" alt="logo"/>
+
 
       </div>
-
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button>login</button>
     </nav>
     
 
